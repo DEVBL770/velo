9ae2b18c1fa3731b370c309d2931e2f0c86420bc
Votre Nom <befrigeo@gmail.com>
2025-09-03 15:37:48 +0200

    Prêt pour le déploiement initial 2

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+// MODIFIÉ : Import du bon paquet
+import prerender from 'vite-plugin-prerender'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    // MODIFIÉ : Configuration du bon paquet
+    prerender({
+      // Le dossier où se trouve le site buildé
+      staticDir: 'dist',
+      // Les routes à pré-rendre
+      routes: ['/'],
+    }),
+  ],
 })
